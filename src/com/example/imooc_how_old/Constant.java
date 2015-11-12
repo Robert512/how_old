dce7fdaa641096c6edc320957a3514e07c654704
Jerry <1275011101@qq.com>
2015-11-12 17:40:10 +0800

    first commit.

---
+++ src/com/example/imooc_how_old/Constant.java
@@ -0,0 +1,8 @@
+package com.example.imooc_how_old;
+
+public class Constant {
+	
+	public static final String KEY = "7aff54fd33fe997b2351fe1249161230";
+	public static final String SECRET = "ZQawKyvBBOgVeflgOm-ayGV3_CmeQskN";
+
+}
---
+++ src/com/example/imooc_how_old/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.imooc_how_old;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	
+	private static final int PICK_CODE = 0x110;
+	private ImageView mPhoto;
+	private Button mGetImage;
+	private Button mDetect;
+	private TextView mTip;
+	private View mWaiting;
+	
+	private String mCurrentPhotoStr;
+	
+	private Bitmap mPhotoImg;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		initViews();
+		initEvents();
+		
+	}
+
+	private void initEvents() {
+		mGetImage.setOnClickListener(this);
+		mDetect.setOnClickListener(this);
+		
+	}
+
+	private void initViews() {
+		
+		mPhoto = (ImageView) findViewById(R.id.id_photo);
+		mGetImage = (Button) findViewById(R.id.id_getImage);
+		mDetect = (Button) findViewById(R.id.id_detect);
+		mTip = (TextView) findViewById(R.id.id_tip);
+		mWaiting = findViewById(R.id.id__waiting);
+		
+		
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		
+		if(requestCode == PICK_CODE)
+		{
+			if(intent != null)
+			{
+				//通过游标来获取图片的路径
+				Uri uri = intent.getData();
+				Cursor cursor = getContentResolver().query(uri,null,null,null,null);
+				cursor.moveToFirst();
+				
+				int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+				mCurrentPhotoStr = cursor.getString(idx);
+				cursor.close();
+				
+				//图片二进制获取的数据不能大于3M，根据照片的尺寸来压缩
+				resizePhoto();
+				
+				mPhoto.setImageBitmap(mPhotoImg);
+				mTip.setText("Click Detect ==>");
+				
+			}
+		}
+		
+		super.onActivityResult(requestCode, resultCode, intent);
+	}
+	
+	private void resizePhoto()
+	{
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(mCurrentPhotoStr,options);
+		
+		double ratio = Math.max(options.outWidth *1.0d /1024f, options.outHeight * 1.0d / 1024f);
+		
+		options.inSampleSize = (int)Math.ceil(ratio);
+		options.inJustDecodeBounds = false;  //加载图片
+
+		mPhotoImg = BitmapFactory.decodeFile(mCurrentPhotoStr, options);
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId())
+		{
+		case R.id.id_getImage:
+			Intent intent = new Intent(Intent.ACTION_PICK);
+			intent.setType("image/*");
+			startActivityForResult(intent, PICK_CODE);
+			
+		}
+		
+	}
+
+	
+
+}
