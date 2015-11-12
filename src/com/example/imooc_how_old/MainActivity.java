6eb5ca9a6f81d3a0f41d8df571d3d8233956a3f6
Jerry <1275011101@qq.com>
2015-11-12 21:26:02 +0800

    second commit.

--- src/com/example/imooc_how_old/MainActivity.java
+++ src/com/example/imooc_how_old/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.imooc_how_old;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.facepp.error.FaceppParseException;
@@ -9,9 +11,15 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -31,6 +39,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	private String mCurrentPhotoStr;
 	
 	private Bitmap mPhotoImg;
+	private Paint mPaint;
 	
 	
 	@Override
@@ -41,14 +50,11 @@ public class MainActivity extends Activity implements OnClickListener {
 		initViews();
 		initEvents();
 		
-	}
-
-	private void initEvents() {
-		mGetImage.setOnClickListener(this);
-		mDetect.setOnClickListener(this);
+		mPaint = new Paint();
 		
 	}
 
+	
 	private void initViews() {
 		
 		mPhoto = (ImageView) findViewById(R.id.id_photo);
@@ -56,9 +62,16 @@ public class MainActivity extends Activity implements OnClickListener {
 		mDetect = (Button) findViewById(R.id.id_detect);
 		mTip = (TextView) findViewById(R.id.id_tip);
 		mWaiting = findViewById(R.id.id__waiting);
-		
+	
+	}
+	
+	
+	private void initEvents() {
+		mGetImage.setOnClickListener(this);
+		mDetect.setOnClickListener(this);
 		
 	}
+
 	
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
@@ -90,6 +103,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	
 	private void resizePhoto()
 	{
+		Log.d("TAG","resizePhoto");
 		BitmapFactory.Options options = new BitmapFactory.Options();
 		options.inJustDecodeBounds = true;
 		BitmapFactory.decodeFile(mCurrentPhotoStr,options);
@@ -100,35 +114,201 @@ public class MainActivity extends Activity implements OnClickListener {
 		options.inJustDecodeBounds = false;  //加载图片
 
 		mPhotoImg = BitmapFactory.decodeFile(mCurrentPhotoStr, options);
+		Log.d("TAG","resizePhoto1");
 		
 	}
+	
+	
+	private static final int MSG_SUCESS = 0x111;
+	private static final int MSG_ERROR = 0x112;
+	
+	
+	private Handler mHandler = new Handler(){
+		
+		//强烈建议写上@Override,因为我写handleMessage出错了
+		@Override
+		public void handleMessage(Message msg)
+		{
+			switch(msg.what)
+			{
+			case MSG_SUCESS:
+				Log.d("TAG","sucess1");
+				mWaiting.setVisibility(View.GONE);
+				Log.d("TAG","sucess2");
+				JSONObject rs = (JSONObject) msg.obj;
+				
+				prepareRsBitmap(rs);
+				
+				mPhoto.setImageBitmap(mPhotoImg);
+				
+				break;
+				
+			case MSG_ERROR:
+				mWaiting.setVisibility(View.GONE);
+
+				String errorMsg = (String) msg.obj;
+				if(TextUtils.isEmpty(errorMsg))
+				{
+					mTip.setText("Error.");
+				}else
+				{
+					mTip.setText(errorMsg);
+					
+				}
+				
+				break;
+			default :
+				break;
+			}
+		}
+	};
+	
 
 	@Override
 	public void onClick(View v) {
 		switch(v.getId())
 		{
 		case R.id.id_getImage:
+			Log.d("TAG","getImage1");
 			Intent intent = new Intent(Intent.ACTION_PICK);
 			intent.setType("image/*");
 			startActivityForResult(intent, PICK_CODE);
+			Log.d("TAG","getImage2");
+			break;   //真的太他妈坑了！！！
 		case R.id.id_detect:
+			Log.d("TAG","detect1");
+			mWaiting.setVisibility(View.VISIBLE);
+			
+			
+			if(mCurrentPhotoStr != null && !mCurrentPhotoStr.trim().equals(""))
+			{
+				resizePhoto();
+			}else
+			{
+				mPhotoImg = BitmapFactory.decodeResource(getResources(), R.drawable.t4);
+			}
+			
+			
 			FaceppDetect.detect(mPhotoImg, new FaceppDetect.CallBack() {
 				
 				@Override
 				public void success(JSONObject result) {
-					// TODO Auto-generated method stub
+					Log.d("TAG","sucess");
+					Message msg = Message.obtain();
+					msg.what = MSG_SUCESS;
+					msg.obj = result;
+					mHandler.sendMessage(msg);
 					
 				}
 				
 				@Override
 				public void error(FaceppParseException exception) {
-					// TODO Auto-generated method stub
+					Log.d("TAG","error");
+					Message msg = Message.obtain();
+					msg.what = MSG_ERROR;
+					msg.obj = exception.getErrorMessage();
+					mHandler.sendMessage(msg);
 					
 				}
 			});
-			
+			break;   //真的太他妈坑了！！！
+		default:
+			break;
 		}
 		
+	}
+
+	protected void prepareRsBitmap(JSONObject rs)
+	{
+		
+		Bitmap bitmap = Bitmap.createBitmap(mPhotoImg.getWidth(),mPhotoImg.getHeight(),mPhotoImg.getConfig());
+		Canvas canvas = new Canvas(bitmap);
+		
+		canvas.drawBitmap(mPhotoImg, 0, 0, null);
+		
+		try {
+			JSONArray faces = rs.getJSONArray("face");
+			
+			int faceCount = faces.length();
+			mTip.setText("find" + faceCount);
+			
+			for(int i=0;i<faceCount;i++)
+			{
+				JSONObject face = faces.getJSONObject(i);
+				JSONObject posObj = face.getJSONObject("position");
+				
+				float x =  (float) posObj.getJSONObject("center").getDouble("x");
+				float y =  (float) posObj.getJSONObject("center").getDouble("y");
+				
+				float w = (float) posObj.getDouble("width");
+				float h = (float) posObj.getDouble("height");
+				
+				x = x / 100 * bitmap.getWidth();
+				y = y / 100 * bitmap.getHeight();
+				
+				w = w / 100 * bitmap.getWidth();
+				h = h / 100 * bitmap.getHeight();
+				
+				mPaint.setColor(0xffffffff);
+				mPaint.setStrokeWidth(3);
+						
+				//画box
+				canvas.drawLine(x - w /2, y - h /2, x - w / 2, y + h /2, mPaint);
+				canvas.drawLine(x - w /2, y - h /2, x + w / 2, y - h /2, mPaint);
+				canvas.drawLine(x + w /2, y + h /2, x - w / 2, y + h /2, mPaint);
+				canvas.drawLine(x + w /2, y + h /2, x + w / 2, y - h /2, mPaint);
+				
+				int age = face.getJSONObject("attribute").getJSONObject("age").getInt("value");
+				String gender = face.getJSONObject("attribute").getJSONObject("age").getString("value");
+				
+				Bitmap ageBitmap = buildAgeBitMap(age,"Male".equals(gender));
+				
+				
+				//编写缩放的代码
+				int ageWidth = ageBitmap.getWidth();
+				int ageHeight = ageBitmap.getHeight();
+				
+				if(bitmap.getWidth() < mPhoto.getWidth() && bitmap.getHeight() < mPhoto.getHeight())
+				{
+					float ratio = Math.max(bitmap.getWidth() * 1.0f/mPhoto.getWidth(), bitmap.getHeight() * 1.0f/mPhoto.getHeight());
+				
+					ageBitmap = Bitmap.createScaledBitmap(ageBitmap, (int)(ageWidth * ratio), (int)(ageHeight * ratio), false);  //这里要用int类型
+	
+					
+				}
+				
+				canvas.drawBitmap(ageBitmap, x - ageBitmap.getWidth(), y - h/2 - ageBitmap.getHeight(), null);
+				
+						
+				
+				mPhotoImg = bitmap;
+			}
+		} catch (JSONException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+
+	private Bitmap buildAgeBitMap(int age, boolean isMale) {
+		TextView tv = (TextView) mWaiting.findViewById(R.id.id_age_and_gender);
+		tv.setText(age + "");
+		if(isMale)
+		{
+			tv.setCompoundDrawablesWithIntrinsicBounds(getResources().getDrawable(R.drawable.male),null,null,null);
+			
+		}else
+		{
+			tv.setCompoundDrawablesWithIntrinsicBounds(getResources().getDrawable(R.drawable.female),null,null,null);
+		}
+		
+		tv.setDrawingCacheEnabled(true);
+		Bitmap bitmap = Bitmap.createBitmap(tv.getDrawingCache());
+		tv.destroyDrawingCache();
+		
+		return bitmap;
 	}
 
 	
